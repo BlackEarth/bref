9739f59c68e0a7a236cb2a553340a9f2abf98fd7
Sean Harrison <sah@blackearth.us>
2016-04-07 22:00:30 -0500

    package files

---
+++ setup.py
@@ -0,0 +1,18 @@
+
+import os, json
+from setuptools import setup, find_packages
+from codecs import open
+
+path = os.path.dirname(os.path.abspath(__file__))
+
+with open(os.path.join(path, 'README.md'), encoding='utf-8') as f:
+    read_me = f.read()
+
+with open(os.path.join(path, 'setup.json'), encoding='utf-8') as f:
+    config = json.loads(f.read())
+
+setup(
+    long_description=read_me,
+    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+    **config
+)
