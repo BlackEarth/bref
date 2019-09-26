127969f1f8606ae5748c67a6e4117c11c9ff93c2
Sean Harrison <sah@bookgenesis.com>
2019-09-25 22:03:34 -0500

    barely working refpat, with NLT pattern_strict

--- bref/refpat.py
+++ bref/refpat.py
@@ -2,66 +2,97 @@ DEBUG = False
 
 import re, os, sys
 
+
 def book_pattern(canon):
-    return r"(?:(?:" + r"|".join([bk.pattern for bk in canon.books]) + r")\.?)"
+    return "(?:(?:" + "|".join([bk.pattern_strict for bk in canon.books]) + ")\\.?)"
+
 
 def book_replacer(canon, attr, flags=re.I):
     """create a function that can be used in re.sub() to replace a found book name with the given book attribute"""
+
     def br(md):
         text = md.group(0)
         for book in canon.books:
-            if re.match(book.pattern, text, flags=flags) is not None:
+            if re.match(book.pattern_strict, text, flags=flags) is not None:
                 return book[attr]
+
     return br
+
 
 def make_patterns(canon):
     # == Build the regexps for finding references in text ==
     patterns = []
-    repeating = []  # list of indexes of patterns that should be repeated until the result is the same as the input
+    repeating = (
+        []
+    )  # list of indexes of patterns that should be repeated until the result is the same as the input
 
     # building blocks
     bkpat = book_pattern(canon)
-    fullbkpat = r"\\b(?:" \
-                + r"|".join([bk.title % bk for bk in canon.books]) \
-                + r")\\b"
-    chpat = r"(?:[1-9][0-9]*[a-f]{0,2}\\b)"
-    vspat = r"(?:[\.:]?[1-9][0-9]*[a-f]{0,2}\\b)"
-    sepat = r"\\s*(?:[,\-\u2013\u2014]?)+\\s*"
-    sepatand = r"\\s*(?:[,\-\u2013\u2014]?(?: and)?)+\\s*"
+    fullbkpat = "\\b(?:" + "|".join([bk.title % bk for bk in canon.books]) + ")\\b"
+    chpat = "(?:[1-9][0-9]*[a-f]{0,2}\\b)"
+    vspat = "(?:[\\.:]?[1-9][0-9]*[a-f]{0,2}\\b)"
+    sepat = "\\s*(?:[,\\-\u2013\u2014]?)+\\s*"
+    sepatand = "\\s*(?:[,\\-\u2013\u2014]?(?: and)?)+\\s*"
 
-    # == patterns == 
+    # == patterns ==
 
     # * full pattern
-    patterns += [r"(?<!>)(" + bkpat + r"\\s*" + chpat + vspat + r"?(" + sepat + bkpat + r"?\\s*" + chpat + vspat + r"?)*" \
-                + r"|" + chpat + vspat + r"(?:" + sepat + chpat + vspat + r"?)*)(?!</a>)"]
+    patterns += [
+        "(?<!>)("
+        + bkpat
+        + "\\s*"
+        + chpat
+        + vspat
+        + "?("
+        + sepat
+        + bkpat
+        + "?\\s*"
+        + chpat
+        + vspat
+        + "?)*"
+        + "|"
+        + chpat
+        + vspat
+        + "(?:"
+        + sepat
+        + chpat
+        + vspat
+        + "?)*)(?!</ref>)"
+    ]
 
     # * "chapters" + chapter nums
-    patterns += [r"\\b([Cc]hapters?\\s*" + chpat + r"(?:" + sepat + chpat + r")*)"]
-    patterns += [r"\\b([Cc]haps?\\.?\\s*" + chpat + r"(?:" + sepat + chpat + r")*)"]
-    patterns += [r"\\b([Cc]hs?\\.?\\s*" + chpat + r"(?:" + sepat + chpat + r")*)"]
+    patterns += ["\\b([Cc]hapters?\\s*" + chpat + "(?:" + sepat + chpat + ")*)"]
+    patterns += ["\\b([Cc]haps?\\.?\\s*" + chpat + "(?:" + sepat + chpat + ")*)"]
+    patterns += ["\\b([Cc]hs?\\.?\\s*" + chpat + "(?:" + sepat + chpat + ")*)"]
 
     # * chapters without book names, after link and semi-colon
-    patterns += [r"(?<=</a>; )(" + chpat + r"(?:" + sepat + chpat + r")?)"]
+    patterns += ["(?<=</ref>; )(" + chpat + "(?:" + sepat + chpat + ")?)"]
     repeating += [patterns.index(patterns[-1])]
 
-    # * chapter number alone, followed by semi-colon and another bref
-    patterns += [r"(" + chpat + r""")(?=;\\s*<a href="\?bref=)"""]
+    # * chapter number alone, followed by semi-colon and another ref
+    patterns += ["(" + chpat + """)(?=;\\s*<ref name=")"""]
     repeating += [patterns.index(patterns[-1])]
 
     # compile all the patterns
-    regexs = [re.compile(pat, re.U) for pat in patterns]
+    regexs = [re.compile(pat) for pat in patterns]
 
     return {'patterns': patterns, 'repeating': repeating, 'regexs': regexs}
+
 
 def tag_refs_in_text(text, patterns, refparser=None):
     def repl_bref(md):
         txt = md.group(1)
         if refparser is not None:
-            refstr = refparser.refstring(refparser.parse(txt))
-            tagged = r"""<ref name="%s">%s</ref>""" % (refstr, txt)
+            try:
+                refstr = refparser.refstring(refparser.parse(txt))
+            except:
+                print("ERR:", txt)
+                return txt
+            tagged = """<ref name="%s">%s</ref>""" % (refstr, txt)
         else:
-            tagged = r"""<ref>%s</ref>""" % (txt,)
+            tagged = """<ref>%s</ref>""" % (txt,)
         return tagged
+
     # get the first item ref to provide a book as context
     for regex in patterns['regexs']:
         if patterns['regexs'].index(regex) in patterns['repeating']:
@@ -71,20 +102,26 @@ def tag_refs_in_text(text, patterns, refparser=None):
                 t = re.sub(regex, repl_bref, text)
         else:
             text = re.sub(regex, repl_bref, text)
+
     return text
 
-def tag_refs_in_xml(x, patterns, xpath=None, namespaces=None):
+
+def tag_refs_in_xml(x, patterns, xpath=None, namespaces=None, refparser=None):
     if xpath is None:
         elements = x.xpath(x.root, "//*")
     else:
         elements = x.xpath(x.root, xpath, namespaces=namespaces)
     for element in elements:
         if element.text is not None and element.get('href') is None:
-            element.text = tag_refs_in_text(element.text, patterns)
+            element.text = tag_refs_in_text(element.text, patterns, refparser=refparser)
         if element.tail is not None:
-            element.tail = tag_refs_in_text(element.tail, patterns)
-    t = x.tostring().replace("&lt;ref&gt;", "<ref>").replace("&lt;/ref&gt;", "</ref>")
-    x.root = x.fromstring(t)
+            element.tail = tag_refs_in_text(element.tail, patterns, refparser=refparser)
+    t = re.sub("&lt;(ref[^&>]+)&gt;", r"<\1>", x.tostring()).replace(
+        "&lt;/ref&gt;", "</ref>"
+    )
+    try:
+        x.root = x.fromstring(t)
+    except:
+        print(t)
+        raise
     return x
-
-
