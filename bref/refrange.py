649b3e579257f61a1ecbffe2c6825cc24bba126b
Sean Harrison <sah@kruxia.com>
2021-11-02 08:58:03 -0500

    format

--- bref/refrange.py
+++ bref/refrange.py
@@ -1,9 +1,12 @@
-
 import re, functools
+
 
 @functools.total_ordering
 class RefRange(list):
-    """the type of the object returned by RefParser.parse_one() -- a list of two references representing a range."""
+    """
+    the type of the object returned by RefParser.parse_one() -- a list of two
+    references representing a range.
+    """
 
     def __str__(self):
         return "%s-%s" % (str(self[0]), str(self[1]))
@@ -13,24 +16,26 @@ class RefRange(list):
 
     def __lt__(self, other):
         return (self[0] < other[0]) or (
-            (self[0]==other[0]) and 
-                (self[1] > other[1]))   # longer ranges sort first
+            (self[0] == other[0]) and (self[1] < other[1])
+        )  # longer ranges sort first
+
+    def __gt__(self, other):
+        return (self[0] > other[0]) or (
+            (self[0] == other[0]) and (self[1] > other[1])
+        )  # longer ranges sort first
 
     def __eq__(self, other):
-        return (self[0]==other[0]) and (self[1]==other[1])
+        return (self[0] == other[0]) and (self[1] == other[1])
 
     def __hash__(self):
         # if __eq__() true, __hash__() will be the same (though the inverse is not true)
-        return int(re.sub(r'\D', '', self[0].key()+self[1].key()))
+        return int(re.sub('\D', '', self[0].key() + self[1].key()))
 
-    def key(self):
-        k1 = self[0].key()
-        k2 = self[1].key()
-        if k1==k2: 
-            return k1
-        else:
-            return "%s-%s" % (k1, k2)
+    def contains(self, other):
+        return self[0] <= other[0] and self[1] >= other[1]
+
 
 if __name__ == "__main__":
     import doctest
+
     doctest.testmod()
